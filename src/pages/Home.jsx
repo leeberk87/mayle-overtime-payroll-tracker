5d84f6d5db79963d18f45b084ad5db56f80494ce
leeberkman <leeberkman@gmail.com>
2026-03-08 11:38:48 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -289,34 +289,3 @@ export default function Home() {
     </div>
   );
 }
-
-// REMOVE DUPLICATE BELOW
-      {menuOpen && (
-        <AddEntryMenu
-          onClose={() => setMenuOpen(false)}
-          onSelectOvertme={() => { setMenuOpen(false); setFormOpen(true); }}
-          onSelectExpense={() => { setMenuOpen(false); setExpenseFormOpen(true); }}
-        />
-      )}
-
-      {/* Overtime Form Modal */}
-      <OvertimeForm 
-        open={formOpen}
-        onOpenChange={handleFormClose}
-        onSubmit={saveMutation.mutate}
-        settings={settings}
-        isLoading={saveMutation.isPending}
-        editingEntry={editingEntry}
-      />
-
-      {/* Expense Form Modal */}
-      <ExpenseForm
-        open={expenseFormOpen}
-        onOpenChange={(open) => { setExpenseFormOpen(open); if (!open) setEditingExpense(null); }}
-        onSubmit={saveExpenseMutation.mutate}
-        isLoading={saveExpenseMutation.isPending}
-        editingEntry={editingExpense}
-      />
-    </div>
-  );
-}
